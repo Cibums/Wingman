4a0680ca860ea304a9d11c6fb1578f0389f80ace
Cibums <56683094+Cibums@users.noreply.github.com>
2020-07-29 18:32:44 +0200

    Hallå

--- App.js
+++ App.js
@@ -1,23 +1,25 @@
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
-import * as React from 'react';
-import { Platform, StatusBar, StyleSheet, View, Button, CheckBox, Text } from 'react-native';
+import React, {useState, useEffect} from 'react';
+import { Platform, StatusBar, StyleSheet, View, Button, CheckBox, Text, Modal, TextInput } from 'react-native';
 
 import useCachedResources from './hooks/useCachedResources';
-import BottomTabNavigator from './navigation/BottomTabNavigator';
-import LinkingConfiguration from './navigation/LinkingConfiguration';
 
 import LoginScreen from './screens/LoginScreen';
 import RegisterScreen from './screens/RegisterScreen';
 
 import * as firebase from 'firebase';
 import 'firebase/firestore';
-import { render } from 'react-dom';
+import 'firebase/auth';
+
 import WingmanScreen from './screens/WingmanScreen';
 import ProfileScreen from './screens/ProfileScreen';
 import PostsScreen from './screens/PostsScreen';
 import GameStructureScreen from './screens/GameStructureScreen';
 import OwnProfileScreen from './screens/OwnProfileScreen';
+import { TouchableOpacity } from 'react-native-gesture-handler';
+import CommentScreen from './screens/CommentsScreen';
+import ChatScreen from './screens/ChatScreen';
 
 var firebaseConfig = {
   apiKey: "AIzaSyD_R8aHKE6FxC8kzPon9rVCbddRlKa-0hE",
@@ -30,51 +32,100 @@ var firebaseConfig = {
   measurementId: "G-24T2LNCQBZ"
 };
 
+var dbh = null;
+var storage = null;
+
 if (!firebase.apps.length) {
+  console.log("Firebase init");
   firebase.initializeApp(firebaseConfig);
 }
-
+else{
+  dbh = firebase.firestore();
+  storage = firebase.storage();
+}
 const Stack = createStackNavigator();
 
 export default function App(props) {
+
+  console.disableYellowBox = true;
+
+  const renderPage = (pageID) => {
+
+    /*return(
+      <ChatScreen uid="qIfhOvauQ5WV1V6stL2mrf73E502" />
+    )*/
+
+    switch(pageID){
+      case 0:
+        return(
+          <LoginScreen />
+        )
+        break;
+      case 1:
+        return(
+          <RegisterScreen />
+        )
+        break;
+      case 2:
+        return(
+          <WingmanScreen />
+        )
+        break;
+      case 3:
+        return(
+          <PostsScreen />
+        )
+        break;
+      case 4:
+        return(
+          <OwnProfileScreen />
+        )
+        break;
+      case 6:
+        return(
+          <ProfileScreen id={App.currentProfileID} />
+        )
+        break;
+      case 7:
+        return(
+          <ChatScreen user={{}} />
+        )
+    }
+  }
+
+  const [loggedInUID, setLoggedInUID] = useState("");
+  App.setLoggedInID = (id) => {
+    setLoggedInUID(id);
+    App.loggedInUserID = id;
+  }
+
+  App.changePage = (id) => {
+    setPage(id);
+    console.log("changed to " + id);
+  }
+
+  const [currentPage, setPage] = useState(0);
+  const [modalVisible, setModalVisabilty] = useState(false);
+
   const isLoadingComplete = useCachedResources();
-
-  return (
-    <View style={styles.container}>
-      <StatusBar barStyle='light-content' />
-      <RegisterScreen />
-    </View>
-    )
-
-
-  return (<View><Text>ERROR</Text></View>)
 
   if (!isLoadingComplete) {
     return null;
   } else {
 
-    if(firebase.auth().currentUser == null){ //Check if user is Signed in
+    if(firebase.auth().currentUser == null){ //Check if user is Signed in firebase.auth().currentUser == null
       return (
-      <View style={styles.container}>
-        <StatusBar barStyle="dark-content" />
-        <NavigationContainer linking={LinkingConfiguration}>
-          <Stack.Navigator>
-            <Stack.Screen name="Root" component={BottomTabNavigator} />
-          </Stack.Navigator>
-        </NavigationContainer>
-      </View>
-      );
+        <View style={styles.container}>
+          <StatusBar barStyle='light-content' />
+          {renderPage(0)}
+        </View>
+      )
     }
     else{
       return (
         <View style={styles.container}>
-          <StatusBar barStyle="dark-content" />
-          <Button title="efef" onPress={() => addUserInfo()} />
-          <NavigationContainer linking={LinkingConfiguration}>
-            <Stack.Navigator>
-              <Stack.Screen name="Root" component={BottomTabNavigator} />
-            </Stack.Navigator>
-          </NavigationContainer>
+          <StatusBar barStyle='light-content' />
+          {renderPage(currentPage)}
         </View>
       );
     }
@@ -84,6 +135,9 @@ export default function App(props) {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+  },
+  label:{
+      margin: 10,
+      width: 80
   },
 });
